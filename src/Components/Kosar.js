60f1012a3fe60b3219384bd2db61641977d68516
BotkaB <botikabinaka@gmail.com>
2024-10-31 19:37:17 +0100

    readme file és reszponzivitás javítása

--- src/Components/Kosar.js
+++ src/Components/Kosar.js
@@ -1,45 +1,48 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import KosarTermek from "./KosarTermek";
-import { KosarContext } from "../Context/KosarContext";
+import { KosarContext } from "../Context/KosarContext"; // KosarContext importálása
 
-
+// 'Kosar' komponens definiálása
 export default function Kosar() {
+  const { kosar, noveles, csokkentes, torles } = useContext(KosarContext);
 
-  const {kosar, noveles, csokkentes, torles}=useContext(KosarContext);
-  // Végösszeg kiszámítása két tizedesjegyre kerekítve
   const vegosszesen = kosar.reduce(
     (osszes, termek) => osszes + termek.price * termek.mennyiseg,
     0
   ).toFixed(2);
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Kép</th>
-          <th>Termék</th>
-          <th>Ár</th>
-          <th>Mennyiség</th>
-          <th>Összesen</th>
-          <th>Művelet</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* A kosárban lévő termékek megjelenítése */}
-        {kosar.map((termek, index) => (
-          <KosarTermek
-            key={index}
-            termek={termek}
-            noveles={noveles}
-            csokkentes={csokkentes}
-            torles={torles}
-          />
-        ))}
-        <tr>
-          <td colSpan="4">Végösszesen</td> {/* Oszlopok összefogása a végösszeg sorhoz. Ez biztosítja, hogy a "Végösszesen" szöveg szépen igazodjon, és ne legyen elválasztva a végösszeg értékétől. */}
-          <td>{vegosszesen} Ft</td> {/* Végösszeg megjelenítése */}
-        </tr>
-      </tbody>
-    </table>
+    <div className="kosar-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Kép</th>
+            <th>Termék</th>
+            <th>Ár</th>
+            <th>Mennyiség</th>
+            <th>Összesen</th>
+            <th>Művelet</th>
+          </tr>
+        </thead>
+        <tbody>
+          {kosar.map((termek, index) => (
+            <KosarTermek
+              key={index}
+              termek={termek}
+              noveles={noveles}
+              csokkentes={csokkentes}
+              torles={torles}
+            />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4">Végösszesen</td>
+            <td>{vegosszesen} Ft</td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
   );
 }
--- src/Components/Termekek.js
+++ src/Components/Termekek.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Termek from "./Termek";
 
-
-export default function Termekek({ termekLista}) {
+// 'Termekek' komponens definiálása, amely egy prop-ot kap: 'termekLista'
+export default function Termekek({ termekLista }) {
   return (
     <div className="container"> {/* Konténer az elrendezéshez */}
       <div className="row"> {/* Bootstrap rács rendszerének használata */}
         {termekLista.map((kartya, index) => (
-          <div key={index} className="col-md-3 col-sm-6 col-xs-12 mb-4"> {/* Oszlopbeállítások különböző méretű képernyőkhöz és margó hozzáadása */}
-            <Termek kartya={kartya}/> 
+          <div key={index} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4"> {/* Oszlopbeállítások különböző méretű képernyőkhöz és margó hozzáadása */}
+            <Termek kartya={kartya} />
           </div>
         ))}
       </div>
